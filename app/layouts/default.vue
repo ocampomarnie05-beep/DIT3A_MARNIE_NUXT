2b8bf06abb8141ff130b841af6b05155987c114c
STUDENT 15 <ocampomarnie05@gmail.com>
2025-11-12 11:09:46 +0800

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-app>
+         <v-navigation-drawer
+        v-model="drawer"
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="DIT-3A"
+            title="Marnie Ocampo"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click.stop="drawer =! drawer" > </v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+    <v-main>
+        <v-container fluid>
+            <slot/>
+        </v-container>
+    </v-main>
+    </v-app>
+  </div>
+</template>
+ <script setup>
+const drawer = ref(true)
+</script>
